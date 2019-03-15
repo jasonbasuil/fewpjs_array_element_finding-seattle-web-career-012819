c6f2abd77808ffcdb9e6e63d361ff08ceade74cb
jasonbasuil <basuilj@gmail.com>
2019-03-14 20:15:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const testVar = {}
+let cards = ['queen of hearts', 'jack of clubs', 'ten of diamonds', 'ace of spades'];
 
-function testFunc() {
-  return "hi"
+console.log(cards.indexOf('jack of clubs')); //1
+
+let cards = ['queen of hearts', 'jack of clubs', 'ten of diamonds', 'ace of spades'];
+
+console.log(cards.indexOf('jack of clubs', 2  )); // -1
+
+function isOdd(element, index, array) {
+  return (element %2 === 1);
 }
+
+console.log([4, 6, 8, 10].find(isOdd)); // undefined, not found
+console.log([4, 5, 8, 10].find(isOdd)); // 5
+console.log([4, 5, 7, 8, 10].find(isOdd)); // 5
+console.log([4, 7, 5,  8, 10].find(isOdd)); // 7
